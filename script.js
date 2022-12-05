4d19726c4752b5daafb00e6219f3c2cf29045515
Cremona Lambert <cremonalambertmw@gmail.com>
2022-12-05 22:24:02 +0200

    Js version 02

--- script.js
+++ script.js
@@ -3,8 +3,9 @@
     let screen = document.querySelector('.board');
     let buttons = document.querySelectorAll('.item');
     let clear = document.querySelector('.clear');
-    let equal = document.querySelector('.equal');
+    let equal = document.getElementById('equal');
     let del = document.getElementById('del');
+    let on = document.getElementById('on');
 
     // getting values from each button per click.
 
@@ -28,7 +29,27 @@
         let result = screen.innerHTML
         screen.innerHTML = result.slice(0,-4);
     })
-    
+
+    //how to setup the "on" button on click
+    on.addEventListener('click', function(e){
+        if(screen.innerHTML === screen.innerHTML){
+            screen.innerHTML = 0;
+        }else{
+            screen.innerHTML = "";
+        }
+    })
+
+    //how to make the equal button work
+
+    equal.addEventListener('click', function(e){
+        if(screen.innerHTML === screen.innerHTML ){
+            screen.innerHTML = "";
+        }else{
+            let answer = eval(screen.innerHTML);
+            screen.innerHTML = answer;
+        }
+    })
+
      
 })()
 
